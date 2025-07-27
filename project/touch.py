7513ddf2b8833d21fafbe820f80871f410d7caea
Sukhman singh <sunnysaini062001@gmail.com>
2025-07-27 12:44:27 +0530

    Updated File

--- project/touch.py
+++ project/touch.py
@@ -2,4 +2,27 @@ import streamlit as st
 
 def get_in_touch():
   
-  st.title("Get In Touch")
+   st.markdown("""
+        <div style='text-align: center'>
+            <h1>Get In Touch!</h1>       
+        </div>
+      """, unsafe_allow_html=True)
+
+   contact_form= """
+    <form action="https://formsubmit.co/sukhmansinghcodes@gmail.com" method="POST">
+      <input type="hidden" name="_captcha" value="false">
+      <input type="text" name="name" placeholder="your name" required>
+      <input type="email" name="email" placeholder="your email" required>
+      <textarea name="message" placeholder="Write your query here"></textarea>
+      <button type="submit">Send</button>
+    </form>
+
+  """ 
+   st.markdown(contact_form, unsafe_allow_html=True) 
+
+   #inject the css using local css
+   def local_css(file_name):
+      with open(file_name) as f:
+         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+   local_css("style/style.css")   
